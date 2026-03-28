3bf91dba3629731aed16cac8bdabbcad959222f8
OrionC3 <93842749+OrionC3@users.noreply.github.com>
2026-03-28 14:11:05 +0100

    Add Item entitie

---
+++ StockApplication.API/Entities/Item.cs
@@ -0,0 +1,20 @@
+﻿namespace StockApplication.API.Entities
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Category { get; set; } = null!;
+
+        public string? Brand { get; set; }
+        public string? Model { get; set; }
+        public string? SerialNumber { get; set; }
+
+        public string? Location { get; set; }
+        public bool Status { get; set; }
+
+
+        public NetworkConfig? Network {  get; set; }
+        public HardwareSpecs? Specs { get; set; }
+    }
+}
