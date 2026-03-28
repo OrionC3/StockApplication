d35fe2b738cac4a8cefca5c506c4fb7c7c6e6dbd
OrionC3 <93842749+OrionC3@users.noreply.github.com>
2026-03-28 15:02:33 +0100

    Create DbContext

---
+++ StockApplication.API/Context/StockApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StockApplication.API.Entities;
+
+namespace StockApplication.API.Context
+{
+    public class StockApplicationContext : DbContext
+    {
+        public StockApplicationContext(DbContextOptions<StockApplicationContext> options) : base(options) { }
+
+        public DbSet<Item> Item => Set<Item>();
+        public DbSet<NetworkSpecs> NetworkSpecs => Set<NetworkSpecs>();
+        public DbSet<HardwareSpecs> HardwareSpecs => Set<HardwareSpecs>();
+    }
+}
