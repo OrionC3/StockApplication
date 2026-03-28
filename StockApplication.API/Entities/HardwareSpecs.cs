b875ff8e160e0daaf146c77118bdeb7c75f89b0a
OrionC3 <93842749+OrionC3@users.noreply.github.com>
2026-03-28 14:33:31 +0100

    Create HardwareSpecs entite

---
+++ StockApplication.API/Entities/HardwareSpecs.cs
@@ -0,0 +1,15 @@
+﻿namespace StockApplication.API.Entities
+{
+    public class HardwareSpecs
+    {
+        public int Id { get; set; }
+        public int ItemId { get; set; }
+        public string? Cpu { get; set; }
+        public string? Ram { get; set; }
+        public string? Storage { get; set; }
+        public string? GraphicCard { get; set; }
+        public string? Allimentation { get; set; }
+
+        public Item? Item { get; set; }
+    }
+}
