1a9f3cd3e813a9215fa86183a66cd2db641c3d00
OrionC3 <93842749+OrionC3@users.noreply.github.com>
2026-03-28 22:44:43 +0100

    Create NetworkSpecsConfig & create ItemController = "add create methode"

---
+++ StockApplication.API/Configs/NetworkSpecsConfig.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StockApplication.API.Entities;
+
+namespace StockApplication.API.Configs
+{
+    public class NetworkSpecsConfig : IEntityTypeConfiguration<NetworkSpecs>
+    {
+        public void Configure(EntityTypeBuilder<NetworkSpecs> builder)
+        {
+            builder.HasKey(n => n.Id);
+            builder.Property(n => n.IpAddress)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            builder.Property(n => n.IpMask)
+                .IsRequired()
+                .HasMaxLength(255);
+            builder.Property(n => n.IpGateway)
+                .IsRequired()
+                .HasMaxLength(255);
+            builder.Property(n => n.MacAddress)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            builder.HasOne(n => n.Item)
+                .WithOne(i => i.NetworkSpec)
+                .HasForeignKey<NetworkSpecs>(n => n.ItemId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ StockApplication.API/Controllers/ItemController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SQLitePCL;
+using StockApplication.API.Context;
+using StockApplication.API.Entities;
+
+namespace StockApplication.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemController : ControllerBase
+    {
+        private readonly StockApplicationContext _context;
+
+        public ItemController(StockApplicationContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult Add([FromBody] Item item)
+        {
+            _context.Add(item);
+            _context.SaveChanges();
+            return Ok(item);
+        }
+    }
+}
